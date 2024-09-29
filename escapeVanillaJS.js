1750d4fc5603de4c5f8b71abaa148488cbc57a2c
NeoMashego <mashegonoj@gmail.com>
2024-09-29 12:43:13 +0200

    added setTimeout function to resolve and wait 1 second to log steps from navigation bar

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -42,9 +42,11 @@ function findIntersection(setA, setB) {
 
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
-        // 🪲 Bug: No delay
-        new Promise(resolve => setTimeout(resolve, 1000));
-        console.log(`Navigating: ${direction.step}`);
+        // 🪲 Bug: No delay present
+        await new Promise(resolve => setTimeout(() => {
+            console.log(`Navigating: ${direction.step}`);
+            resolve();
+        }, 1000));
     }
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
 }
