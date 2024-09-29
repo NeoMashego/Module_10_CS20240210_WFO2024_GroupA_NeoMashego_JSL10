12dfa10ff500ad52ad8c938415f043c8a1e49847
NeoMashego <mashegonoj@gmail.com>
2024-09-29 10:31:27 +0200

    Corrected logic to findIntersection function

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -36,7 +36,7 @@ function findMostRecentBook(books) {
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
-    const intersection = new Set([...setA]);
+    const intersection = new Set([...setA]).filter(con => setB.has(con));
     return intersection;
 }
 
